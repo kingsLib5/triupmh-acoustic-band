aabbde8aee7e72d7792181d589407e520439f169
kingsLib5 <libertykings88@gmail.com>
2025-04-30 16:36:34 +0100

    update

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import Hero from './components/Hero'
 import Music from './components/Music'
 import Video from './components/Video'
-import UpcomingsShows from './components/UpcomingsShows'
+// import UpcomingsShows from './components/UpcomingsShows'
 import Bio from './components/Bio'
 import ContactMe from './components/ContactMe'
 import NavBar from './components/NavBar'
@@ -16,7 +16,7 @@ createRoot(document.getElementById('root')).render(
     <Hero />
     <Music />
     <Video />
-    <UpcomingsShows />
+    {/* <UpcomingsShows /> */}
     <Bio />
     <ContactMe />
     <Footer />
